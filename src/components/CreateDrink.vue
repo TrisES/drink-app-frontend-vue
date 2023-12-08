eb650c09d1a80826e5c841eac5b686cc434f8dd0
TrisES <112662675+TrisES@users.noreply.github.com>
2023-12-08 11:31:02 +0100

    Mulighed for fejl ved alhoholisk drink: ja/nej. fikset.

--- src/components/CreateDrink.vue
+++ src/components/CreateDrink.vue
@@ -120,7 +120,7 @@ export default {
                 "strVideo": null,
                 "strCategory": "Cocktail",
                 "strIBA": null,
-                "strAlcoholic": "",
+                "strAlcoholic": "Alcoholic",
                 "strGlass": null,
                 "strInstructions": "",
                 "strDrinkThumb": "",
