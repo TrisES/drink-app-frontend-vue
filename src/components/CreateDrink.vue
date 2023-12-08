1570b882b742c015ead96d0ec25ab44e4685c298
TrisES <112662675+TrisES@users.noreply.github.com>
2023-12-08 10:56:08 +0100

    REST service link ændret til azure/online.

--- src/components/CreateDrink.vue
+++ src/components/CreateDrink.vue
@@ -158,7 +158,7 @@ export default {
             if (this.ValidateModel()) {
                 try {
                     console.log(this.DrinkModel)
-                    let responsePost = await axios.post('http://localhost:5002/api/DrinkModel', this.DrinkModel)
+                    let responsePost = await axios.post('https://drinksmaskinerest.azurewebsites.net/api/DrinkModel', this.DrinkModel)
                     // console.log(response.status + " " + response.statusText)
                     if (responsePost.status == 200) {
                         alert("Drink created")
