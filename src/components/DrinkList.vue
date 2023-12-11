b500a763df6f5c860f0e348557c42d601472698c
daoudahmad <daoud@hotmail.dk>
2023-12-11 13:06:52 +0100

    d

--- src/components/DrinkList.vue
+++ src/components/DrinkList.vue
@@ -115,7 +115,7 @@ export default {
             currentDrink: null,
             searchToGetBy: "",
             searchToGetByForIngredient: "",
-            currentPae: "liste"
+            currentPage: "liste"
         };
     },
     async created() {
