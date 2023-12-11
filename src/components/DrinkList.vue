27410c98db78b4c0eaf88a276412fc6021cf9e0c
Jeppe <Olsen1906@gmail.com>
2023-12-11 13:01:49 +0100

    slet

--- src/components/DrinkList.vue
+++ src/components/DrinkList.vue
@@ -1,7 +1,7 @@
 <template>
     <nav v-if="currentPage == 'liste'" class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
         <div class="container-fluid">
-            <a class="navbar-brand" href="">Drinks</a>
+            <a class="navbar-brand" href="">Drinkss</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03"
                 aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
